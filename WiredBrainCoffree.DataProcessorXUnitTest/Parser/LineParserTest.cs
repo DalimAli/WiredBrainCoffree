0ca4ae03ed8f953f02e4a93a12b7b68b9a4546da
DalimAli <dalim3281@gmail.com>
2023-04-02 00:13:29 +0600

    first x unit test completed

---
+++ WiredBrainCoffree.DataProcessorXUnitTest/Parser/LineParserTest.cs
@@ -0,0 +1,21 @@
+﻿using WiredBrainCoffree.DataProcess.Parsing;
+
+namespace WiredBrainCoffree.DataProcessorXUnitTest.Parser
+{
+    public class LineParserTest
+    {
+        [Fact]
+        public void ShouldParseValidLine()
+        {
+            string[] lines = new[] { "Express;10/2/2022 8:22:16 AM" };
+           var machineDataItems = LineParser.Parse(lines);
+
+            Assert.NotNull(machineDataItems);
+            Assert.Single(machineDataItems);
+            Assert.Equal("Express", machineDataItems[0].CoffeeType);
+            Assert.Equal(new DateTime(2022,10, 2, 8, 22, 16), machineDataItems[0].CreatedAt);
+
+
+        }
+    }
+}
