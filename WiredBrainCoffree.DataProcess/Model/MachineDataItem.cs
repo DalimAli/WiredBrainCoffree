0b61226f50f30f013e4d13ee8d1c92808471d36f
DalimAli <dalim3281@gmail.com>
2023-04-01 23:44:05 +0600

    Record type implemented

--- WiredBrainCoffree.DataProcess/Model/MachineDataItem.cs
+++ WiredBrainCoffree.DataProcess/Model/MachineDataItem.cs
@@ -6,15 +6,5 @@ using System.Threading.Tasks;
 
 namespace WiredBrainCoffree.DataProcess.Model
 {
-    public class MachineDataItem
-    {
-        public MachineDataItem(string coffeeType, DateTime createdAt)
-        {
-            this.CoffeeType = coffeeType;
-            this.CreatedAt = createdAt;
-        }
-
-        public string CoffeeType { get; }
-        public DateTime CreatedAt { get; }
-    }
+    public record MachineDataItem(string CoffeeType, DateTime CreatedAt);
 }
